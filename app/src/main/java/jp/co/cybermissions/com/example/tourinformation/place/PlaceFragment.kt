f085af593c51d4984d94ed0023ed009e24ea20de
Win War Phoo <myominoo@mm.cybermissions.co.jp>
2020-12-24 13:20:54 +0900

    Navigation

--- app/src/main/java/jp/co/cybermissions/com/example/tourinformation/place/PlaceFragment.kt
+++ app/src/main/java/jp/co/cybermissions/com/example/tourinformation/place/PlaceFragment.kt
@@ -1,24 +1,19 @@
 package jp.co.cybermissions.com.example.tourinformation.place
 
 import android.os.Bundle
-import android.util.Log
 import android.view.*
-import android.widget.Button
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+
 import jp.co.cybermissions.com.example.tourinformation.R
 import jp.co.cybermissions.com.example.tourinformation.databinding.PlaceBinding
 
 class PlaceFragment  : Fragment() {
-//private lateinit var binding : PlaceBinding
 
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        //setHasOptionsMenu(true)
-//    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,11 +22,11 @@ class PlaceFragment  : Fragment() {
     ): View? {
         val binding: PlaceBinding = DataBindingUtil.inflate(inflater,R.layout.place, container, false)
 
-
-        binding.place1.setOnClickListener {view: View ->
+        binding.place1.setOnClickListener { view: View ->
             view.findNavController()
                 .navigate(PlaceFragmentDirections.actionPlaceFragmentToPlacedetailFragment())
         }
+
         binding.place2.setOnClickListener {view: View ->
             view.findNavController()
                 .navigate(PlaceFragmentDirections.actionPlaceFragmentToPlacedetailFragment())
@@ -40,21 +35,18 @@ class PlaceFragment  : Fragment() {
             view.findNavController()
                 .navigate(PlaceFragmentDirections.actionPlaceFragmentToPlacedetailFragment())
         }
+
+        //setHasOptionsMenu(true)
         return binding.root
     }
 
+//    fun  buttonClicked() {
+//        name.set("Button Clicked")
+//    }
+
 //    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
 //        super.onCreateOptionsMenu(menu, inflater)
-//        inflater.inflate(R.menu.options_menu, menu)
-//    }
-//
-//    override fun onContextItemSelected(item: MenuItem): Boolean {
-//        when (item.getItemId()) {
-//            R.id.aboutFragment -> {
-//                return true
-//            }
-//            else -> return super.onOptionsItemSelected(item)
-//        }
+//        inflater.inflate(R.menu.menu_about, menu)
 //    }
 
 }
--- app/src/main/java/jp/co/cybermissions/com/example/tourinformation/place_detail/PlaceDetailViewModel.kt
+++ app/src/main/java/jp/co/cybermissions/com/example/tourinformation/place_detail/PlaceDetailViewModel.kt
@@ -1,19 +1,14 @@
 package jp.co.cybermissions.com.example.tourinformation.place_detail
 
+import android.app.Application
 import android.widget.TextView
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.databinding.ObservableField
+import androidx.lifecycle.*
+//import jp.co.cybermissions.com.example.tourinformation.Detail
+import jp.co.cybermissions.com.example.tourinformation.R
+import java.util.*
 
 class PlaceDetailViewModel : ViewModel() {
-
- //  val placedetails: MutableList<PlaceDetail> = mutableListOf(
-//        PlaceDetail(text =
-     var placeList: MutableList<String> = mutableListOf(
-     "London",
-     "NewYork",
-     "Sanfran"
- )
 
     private val _currentplace = MutableLiveData<String>()
     val currentplace: LiveData<String>
@@ -21,7 +16,10 @@ class PlaceDetailViewModel : ViewModel() {
 
     init {
         CurrentPlace()
+        //_selectedProperty.value = detailsProperty
+
     }
+
 
     private fun CurrentPlace() {
             _currentplace.value =
@@ -29,11 +27,6 @@ class PlaceDetailViewModel : ViewModel() {
                         "\n" +
                         "The World Nuclear Association Symposium will be held at Central Hall Westminster. Many of London’s top attractions lie only a short distance from here, such as Trafalgar Square, the National Gallery, Piccadilly Circus, Leicester Square, Buckingham Palace and St. James’s Park. The location is also within London’s “theatreland” and close to many of the capital’s best pubs, clubs, restaurants and shops." +
                         "\n"+"This has always been a powerful place because of its position on the mighty River Thames, with easy access to the Thames estuary and to the ocean beyond. London's docks, now home to trendy apartments, were the key to its prosperity as the world's greatest trading city in the 19th century, at the very height of the British Empire."
-    }
-
-    private fun nextButton() {
-        placeList.contains("London")
-
     }
 }
 
